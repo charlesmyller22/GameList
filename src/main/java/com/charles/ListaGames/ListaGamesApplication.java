c25cac9ba7f2d27fedbefb5df92d596df750eef0
Charles Pereira <charlesmyller@hotmail.com>
2023-05-09 16:23:50 +0200

    Project created

---
+++ src/main/java/com/charles/ListaGames/ListaGamesApplication.java
@@ -0,0 +1,13 @@
+package com.charles.ListaGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charles/ListaGames/ListaGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charles.ListaGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
