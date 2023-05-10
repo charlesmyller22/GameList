799c41e1cb81efd2fb19b85092975bf04e1fa05a
Charles Pereira <charlesmyller@hotmail.com>
2023-05-10 10:38:49 +0200

    Get by id added to project

--- src/main/java/com/charles/ListaGames/services/GameService.java
+++ src/main/java/com/charles/ListaGames/services/GameService.java
@@ -4,16 +4,27 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.charles.ListaGames.dto.GameDTO;
 import com.charles.ListaGames.dto.GameMinDTO;
 import com.charles.ListaGames.entities.Game;
 import com.charles.ListaGames.repositories.GameRepository;
+
 
 @Service
 public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
