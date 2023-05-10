bf1c8989a07ee86c12b5bf3fb02533e06d3b117f
Charles Pereira <charlesmyller@hotmail.com>
2023-05-10 11:58:29 +0200

    Get lists added to project

---
+++ src/main/java/com/charles/ListaGames/services/GameListService.java
@@ -0,0 +1,26 @@
+package com.charles.ListaGames.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.charles.ListaGames.dto.GameListDTO;
+import com.charles.ListaGames.entities.Game;
+import com.charles.ListaGames.entities.GameList;
+import com.charles.ListaGames.repositories.GameListRepository;
+
+
+@Service
+public class GameListService {
+	@Autowired
+	private GameListRepository gameListRepository;
+	
+	@Transactional(readOnly = true)
+	public List<GameListDTO> findAll() {
+		List<GameList> result = gameListRepository.findAll();
+		       
+		return result.stream().map(x -> new GameListDTO(x)).toList();
+	}
+}
